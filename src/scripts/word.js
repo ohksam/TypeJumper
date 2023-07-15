6e5ce85ea719c0c08acd164b17f8f29201b084ca
ohksam <SamOh37@gmail.com>
2023-07-14 20:30:33 -0400

    minor optimization on word.draw

--- src/scripts/word.js
+++ src/scripts/word.js
@@ -16,26 +16,26 @@ class Word {
 
     draw(ctx) {
         ctx.font = '32px CuteFrog';
-    
-        // Calculate the width of the rendered text
+      
         const textWidth = ctx.measureText(this.string).width;
-    
-        // Calculate the x-coordinate to center the text
-        const centerX = this.position.x + (Platform.WIDTH - textWidth) / 2;
-    
-        // Draw the green letters
-        for (let i = 0; i < this.index; i++) {
-            ctx.fillStyle = '#00BD00';
-            const letterX = centerX + ctx.measureText(this.string.slice(0, i)).width;
-            ctx.fillText(this.string[i], letterX, this.position.y + Platform.HEIGHT, Platform.WIDTH);
+        const centerX = this.position.x + Platform.WIDTH / 2 - textWidth / 2;
+      
+        let accumulatedWidth = 0; // Accumulated width of the drawn letters
+      
+        for (let i = 0; i < this.string.length; i++) {
+          const letterState = this.letterStates[i];
+      
+          ctx.fillStyle = letterState === 'green' ? '#00BD00' : '#BF0436';
+      
+          const letterX = centerX + accumulatedWidth;
+          ctx.fillText(this.string[i], letterX, this.position.y + Platform.HEIGHT, Platform.WIDTH);
+      
+          // Add the current letter's width to the accumulated width
+          accumulatedWidth += ctx.measureText(this.string[i]).width;
         }
-    
-        // Draw the remaining red letters
-        ctx.fillStyle = '#BF0436';
-        const remainingText = this.string.slice(this.index);
-        const remainingTextX = centerX + ctx.measureText(this.string.slice(0, this.index)).width;
-        ctx.fillText(remainingText, remainingTextX, this.position.y + Platform.HEIGHT, Platform.WIDTH);
-    }
+      }
+      
+      
     
     update(position) {
         this.position.x = position.x;
