126860f45c13d51fe978c2add643d76c4956f998
ohksam <SamOh37@gmail.com>
2023-07-14 20:14:54 -0400

    refactor word.draw, text centered

--- src/scripts/word.js
+++ src/scripts/word.js
@@ -6,58 +6,47 @@ class Word {
         this.position = {
             x: x,
             y: y
-        }
+        };
         this.index = 0;
-    }
-
-    paintLastGreen(ctx) {
-        if (this.index < this.string.length - 1) {
-            ctx.font = '32px CuteFrog';
-            ctx.fillStyle = 'rgba(255, 255, 255, 0.5)';
-            const textToDraw = this.string.slice(0, this.index);
-            const textWidth = ctx.measureText(textToDraw).width;
-            const centerX = this.position.x + (Platform.WIDTH - textWidth) / 2;
-            ctx.fillText(textToDraw, centerX, this.position.y + Platform.HEIGHT, Platform.WIDTH);
-        } else if (this.index === this.string.length) {
-            ctx.font = '32.px CuteFrog';
-            ctx.fillStyle = '#00BD00';
-            const centerX = this.position.x + (Platform.WIDTH - ctx.measureText(this.string).width) / 2;
-            ctx.fillText(this.string, centerX, this.position.y + Platform.HEIGHT, Platform.WIDTH);
+        this.letterStates = {};
+        for (let i = 0; i < this.string.length; i++) {
+            this.letterStates[i] = "red";
         }
-    }
-
-    drawGreen(ctx) {
-        ctx.font = '32px CuteFrog';
-        ctx.fillStyle = '#00BD00';
-        const textToDraw = this.string.slice(0, this.index);
-        const textWidth = ctx.measureText(textToDraw).width;
-        const centerX = this.position.x + (Platform.WIDTH - textWidth) / 2;
-        ctx.fillText(textToDraw, centerX, this.position.y + Platform.HEIGHT, Platform.WIDTH);
     }
 
     draw(ctx) {
         ctx.font = '32px CuteFrog';
-        ctx.fillStyle = '#BF0436';
-
+    
         // Calculate the width of the rendered text
         const textWidth = ctx.measureText(this.string).width;
-
+    
         // Calculate the x-coordinate to center the text
         const centerX = this.position.x + (Platform.WIDTH - textWidth) / 2;
-
-        ctx.fillText(this.string, centerX, this.position.y + Platform.HEIGHT, Platform.WIDTH);
-
-        this.drawGreen(ctx);
-        this.paintLastGreen(ctx);
+    
+        // Draw the green letters
+        for (let i = 0; i < this.index; i++) {
+            ctx.fillStyle = '#00BD00';
+            const letterX = centerX + ctx.measureText(this.string.slice(0, i)).width;
+            ctx.fillText(this.string[i], letterX, this.position.y + Platform.HEIGHT, Platform.WIDTH);
+        }
+    
+        // Draw the remaining red letters
+        ctx.fillStyle = '#BF0436';
+        const remainingText = this.string.slice(this.index);
+        const remainingTextX = centerX + ctx.measureText(this.string.slice(0, this.index)).width;
+        ctx.fillText(remainingText, remainingTextX, this.position.y + Platform.HEIGHT, Platform.WIDTH);
     }
-
+    
     update(position) {
         this.position.x = position.x;
         this.position.y = position.y;
     }
 
     handleCorrectKey() {
-        this.index += 1;
+        if (this.index < this.string.length) {
+            this.letterStates[this.index] = "green";
+            this.index += 1;
+        }
     }
 
     handleBadKey() {
@@ -66,6 +55,9 @@ class Word {
 
     resetIndex() {
         this.index = 0;
+        for (let i = 0; i < this.string.length; i++) {
+            this.letterStates[i] = "red";
+        }
     }
 }
 
